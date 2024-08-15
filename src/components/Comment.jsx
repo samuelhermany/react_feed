f18205ed7a3d4cb3756ff53edc294bf5a8c22979
samuel <samuelhermany@hotmail.com>
2024-08-15 02:44:42 -0300

    Start avatar component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,4 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 
 export function Comment(){
@@ -5,6 +6,29 @@ export function Comment(){
       <div className={styles.comment}>
          <img src="https://github.com/samuelhermany.png" alt=""/>
 
+         <div className={styles.commentBox}>
+            <div className={styles.commentContent}>
+               <header>
+                  <div className={styles.authorAndTime}>
+                     <strong>Diego Fernandes</strong>
+                     <time title='11 de maio às 08:13h' dateTime='2022-05-11 08:13:30'>Cerca de uma 1h atrás</time>
+                  </div>
+
+                  <button title='Deletar comentário'>
+                     <Trash size={24}/>
+                  </button>
+               </header>
+
+               <p>Muito bom Devon, parabéns!! 👏👏</p>
+            </div>
+
+            <footer>
+               <button>
+                  <ThumbsUp />
+                  Aplaudir <span>20</span>
+               </button>
+            </footer>
+         </div>
      </div>
   )
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post(){
@@ -12,7 +13,7 @@ export function Post(){
                </div>
             </div>
 
-            <time title='11 de maio às 08:13' dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
+            <time title='11 de maio às 08:13h' dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
          </header>
 
          <div className={styles.content}>
@@ -36,6 +37,12 @@ export function Post(){
                <button type='submit'>Publicar</button>
             </footer>
          </form>
+
+         <div className={styles.commentList}>
+            <Comment />
+            <Comment />
+            <Comment />
+         </div>
      </article>
   )
 }
