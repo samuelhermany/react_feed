f4034b19fdcac14cea307cf20f55dcd2354b0d7b
samuel <samuelhermany@hotmail.com>
2024-08-15 01:10:20 -0300

    Component Post finished

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -14,12 +14,28 @@ export function Post(){
 
             <time title='11 de maio às 08:13' dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
          </header>
+
          <div className={styles.content}>
             <p>Fala galeraa 👋</p>
             <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-            <p><a href="">👉 jane.design/doctorcare</a></p>
-            <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+            <p><a href="">jane.design/doctorcare</a></p>
+            <p>
+               <a href="">#novoprojeto</a>{' '}
+               <a href="">#nlw</a>{' '}
+               <a href="">#rocketseat</a>{' '}
+            </p>
          </div>
+
+         <form className={styles.commentForm}>
+            <strong>Deixe seu feedback</strong>
+
+            <textarea placeholder="Deixe seu coomentário"
+            />
+
+            <footer>
+               <button type='submit'>Publicar</button>
+            </footer>
+         </form>
      </article>
   )
 }
